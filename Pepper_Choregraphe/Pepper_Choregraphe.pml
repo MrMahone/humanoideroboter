841b3da9711435a5c7bb4d9b740fe6e939b7415c
DustinW <dustinwol@yahoo.de>
2018-11-16 15:49:19 +0100

    Merge branch 'webApp' of C:\Users\Dustin\Projects\humanoideroboter with conflicts.

--- Pepper_Choregraphe/Pepper_Choregraphe.pml
+++ Pepper_Choregraphe/Pepper_Choregraphe.pml
@@ -9,33 +9,14 @@
         <File name="index" src="html/index.html" />
         <File name="Stylesheet" src="html/CSS/Stylesheet.css" />
         <File name="Dialogs" src="html/Dialogs.txt" />
-        <File name="chooseAnimal" src="html/de/chooseAnimal.html" />
-        <File name="index" src="html/de/index.html" />
-        <File name="chicken" src="html/de/Sounds/chicken.mp3" />
-        <File name="cow" src="html/de/Sounds/cow.mp3" />
-        <File name="sheep" src="html/de/Sounds/sheep.mp3" />
-        <File name="lautsprecher" src="html/de/img/lautsprecher.jpg" />
-        <File name="error" src="html/de/img/error.jpg" />
-        <File name="jquery.min" src="html/de/jquery.min.js" />
-        <File name="robotutils" src="html/de/robotutils.js" />
-        <File name="touch" src="html/de/touch.js" />
-        <File name="game" src="html/de/game.js" />
-        <File name="kuh" src="html/de/img/kuh.jpg" />
-        <File name="ratte" src="html/de/img/ratte.jpg" />
-        <File name="Brot" src="html/de/img/Brot.jpg" />
-        <File name="Milch" src="html/de/img/Milch.jpg" />
-        <File name="Bauernhof" src="html/de/img/Bauernhof.JPG" />
-        <File name="Ei" src="html/de/img/Ei.jpg" />
-        <File name="Elefan" src="html/de/img/Elefan.jpg" />
-        <File name="Ente" src="html/de/img/Ente.jpg" />
-        <File name="Esel" src="html/de/img/Esel.jpg" />
-        <File name="Fuchs" src="html/de/img/Fuchs.jpg" />
-        <File name="Heu" src="html/de/img/Heu.jpg" />
-        <File name="Katze" src="html/de/img/Katze.jpg" />
-        <File name="Pferd" src="html/de/img/Pferd.jpg" />
-        <File name="Schaf" src="html/de/img/Schaf.jpg" />
-        <File name="Schweinefleisch" src="html/de/img/Schweinefleisch.jpg" />
-        <File name="Schweineschnitzel" src="html/de/img/Schweineschnitzel.JPG" />
+        <File name="chicken" src="html/Sounds/chicken.mp3" />
+        <File name="cow" src="html/Sounds/cow.mp3" />
+        <File name="sheep" src="html/Sounds/sheep.mp3" />
+        <File name="chooseAnimal" src="html/chooseAnimal.html" />
+        <File name="game" src="html/script/game.js" />
+        <File name="jquery.min" src="html/script/jquery.min.js" />
+        <File name="robotutils" src="html/script/robotutils.js" />
+        <File name="touch" src="html/script/touch.js" />
     </Resources>
     <Topics />
     <IgnoredPaths />
--- Pepper_Choregraphe/html/de/game.js
+++ Pepper_Choregraphe/html/de/game.js
@@ -1,9 +1,5 @@
-alert("juck");
-var idGame; //id für Auswahl der Spielreihe
-var Games = [];
-var leftImg;
-var rightImg;
-var speaker;
+//let session = QiSession();
+
 
 /*   The Game "Object":
 
@@ -17,9 +13,28 @@ var speaker;
            ]
  */
 
+let idGame; //id für Auswahl der Spielreihe
+let Games = [];
+let leftImg = document.getElementById('IimgLeft');
+let rightImg = document.getElementById('IimgRight');
+let speaker = document.getElementById('ISpeaker');
+
+function onButtonSpeakerClick(){
+    let audio = document.getElementById('IAudio');
+    audio.src = Games[idGame].sounds;
+    audio.play();
+    //let speechService = session.service('ALAudioPlayerProxy');
+    //speechService.loadFile('../Sounds/cow.mp3');
+    //speechService.play();
+}
+
+
+function setImgs(){
+    leftImg.src = Games[idGame].GameParts[Games[idGame].solvedParts].leftImg;
+    rightImg.src = Games[idGame].GameParts[Games[idGame].solvedParts].rightImg;
+}
 function onLoad(){
-	
-	alert();
+
     leftImg = document.getElementById('IimgLeft');
     rightImg = document.getElementById('IimgRight');
     speaker = document.getElementById('ISpeaker');
@@ -27,10 +42,41 @@ function onLoad(){
     idGame = 0;
     setImgs();
 }
+function selectNewGame(){
+    // now the first Game is played always. random could be implemented
+    // idGame remains 0
+
+
+    Games.shift();
+    if(Games.length > 0){
+        alert('Neue Runde!');
+        //there is still a Game to select
+        speaker.style.visibility = 'visible';
+        setImgs();
+    } else {
+        //Game is over
+        alert('Game Over!');
+    }
+}
+function onImgClick(pos){
+    if (Games[idGame].GameParts[Games[idGame].solvedParts].correctImg === pos){
+        //correct Image has been selected
+        alert('CORRECT');
+        if(++Games[idGame].solvedParts === Games[idGame].GameParts.length){
+            //all parts solved
+            selectNewGame();
+        } else {
+            //next Part
+            speaker.style.visibility = 'hidden';
+            setImgs();
+        }
+    }
+}
+
 
 function appendGame(leftImgs, rightImgs, correctImgs, sound){
-    var gameParts = [];
-    for(var i=0; i<leftImgs.length; i++){
+    let gameParts = [];
+    for(let i=0; i<leftImgs.length; i++){
         gameParts.push({
             leftImg: leftImgs[i], //String
             rightImg: rightImgs[i], //String
@@ -48,10 +94,10 @@ function appendGame(leftImgs, rightImgs, correctImgs, sound){
 function setGames() {
 
     //Game no. 1: Kuh
-    appendGame(['./img/kuh.jpg','./img/Brot.jpg','./img/1Käse.jpg'],
-        ['./img/ratte.jpg','./img/Milch.jpg','./img/1Wurst.jpg'],
-        ['l','r','l'],
-        './Sounds/cow.mp3');
+    appendGame(['./img/kuh.jpg','./img/Brot.jpg','./img/1Käse.jpg'], //Linke Bilder
+        ['./img/ratte.jpg','./img/Milch.jpg','./img/1Wurst.jpg'],   // Rechte Bilder
+        ['l','r','l'],                                              //richtiges Bild
+        './Sounds/cow.mp3');                                        //Sound (1. Bild)
     //Game no. 2: huhn
     appendGame(['./img/huhn.jpg','./img/Ei.jpg','./img/Apfel.jpg'],
         ['./img/Esel.jpg','./img/Mehl.jpg','./img/1Spiegelei.jpg'],
@@ -59,45 +105,3 @@ function setGames() {
         './Sounds/chicken.mp3');
 }
 
-function setImgs(){
-    leftImg.src = Games[idGame].GameParts[Games[idGame].solvedParts].leftImg;
-    rightImg.src = Games[idGame].GameParts[Games[idGame].solvedParts].rightImg;
-}
-
-function onButtonSpeakerClick(){
-    var audio = document.getElementById('IAudio');
-    audio.src = Games[idGame].sounds;
-    audio.play();
-}
-
-function selectNewGame(){
-    // now the first Game is played always. random could be implemented
-    // idGame remains 0
-
-
-    Games.shift();
-    if(Games.length > 0){
-        alert('Neue Runde!');
-        //there is still a Game to select
-        speaker.style.visibility = 'visible';
-        setImgs();
-    } else {
-        //Game is over
-        alert('Game Over!');
-    }
-}
-
-function onImgClick(pos){
-    if (Games[idGame].GameParts[Games[idGame].solvedParts].correctImg === pos){
-        //correct Image has been selected
-        alert('CORRECT');
-        if(++Games[idGame].solvedParts === Games[idGame].GameParts.length){
-            //all parts solved
-            selectNewGame();
-        } else {
-            //next Part
-            speaker.style.visibility = 'hidden';
-            setImgs();
-        }
-    }
-}
